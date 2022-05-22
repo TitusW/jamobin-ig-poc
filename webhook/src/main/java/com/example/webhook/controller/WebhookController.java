deb120ec19553dd3f548778a11b2747b94690932
TitusW <32323441+TitusW@users.noreply.github.com>
2022-05-22 22:48:14 +0700

    Finish get webhook verification + receiving webhook event

--- webhook/src/main/java/com/example/webhook/controller/WebhookController.java
+++ webhook/src/main/java/com/example/webhook/controller/WebhookController.java
@@ -5,7 +5,11 @@ import com.example.webhook.entity.ClientWebhook;
 import com.example.webhook.entity.MessageIn;
 import com.example.webhook.service.WebhookService;
 import lombok.AllArgsConstructor;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/webhook")
@@ -14,14 +18,27 @@ public class WebhookController {
 
     private final WebhookService webhookService;
 
-//    @GetMapping("{webhook_id}")
-//    public ClientWebhook fetchClientWebhook(@PathVariable("webhook_id") String webhook_id) {
+    @GetMapping()
+    public String fetchClientWebhook(
+            @RequestParam Map<String,String> allRequestParams, ModelMap model
+        ) {
 //        ClientWebhook cw = new ClientWebhook();
-//        return cw;
-//    }
+//        System.out.println(hub_mode);
+        System.out.println("at get");
+        System.out.println(allRequestParams.keySet());
+        System.out.println(allRequestParams.get("hub.challenge"));
+        return allRequestParams.get("hub.challenge");
+    }
 
     @PostMapping()
-    public void postMessage(@RequestBody MessageIn messageIn) {
-        webhookService.postMessage(messageIn);
+    public void post2(RequestEntity<String> request) {
+        String body = request.getBody();
+        System.out.println("at post");
+        System.out.println(body);
     }
+
+//    @PostMapping()
+//    public void postMessage(@RequestBody MessageIn messageIn) {
+//        webhookService.postMessage(messageIn);
+//    }
 }
